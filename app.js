83379bd1540c87fc131a18184cc4e444f4473753
Adarsha Shrestha <30285136+Shrestha01@users.noreply.github.com>
2025-03-11 09:52:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,16 @@ app.delete("/api/menu/delete/:id", async (req, res) => {
     const user = await Menu.findByIdAndDelete(id); // Delete user by ID
     if (!user) {
       return res.status(404).json({
-        message: "User not found",
+        message: "Item not found",
       });
     }
     res.status(200).json({
-      message: `User with deleted successfully`,
+      message: `Item  deleted successfully`,
       user,
     });
   } catch (error) {
     res.status(500).json({
-      message: "Error deleting user",
+      message: "Error deleting Item",
       error: error.message,
     });
   }
